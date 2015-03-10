c9444f210bb0b0ea247d576e8236e9ae7b2d0be2
Imran3 <imranazam3@gmail.com>
2015-03-10 23:34:59 +0100

    added some comments, how to make a test with telnet

--- timeServer.js
+++ timeServer.js
@@ -2,7 +2,9 @@
 creare un server TCP usando modulo net
 net.createServer restituisce un serve e accetta un funzione di callback
 function(socket) è quello che soddisfa le richiete dei clienti
-posso fare socket.write(dati), socket.read(dati)...*/
+posso fare socket.write(dati), socket.read(dati)...
+per fare prova usare telnet
+telnet localhost numeroPortaServer*/
 var net = require("net")	//modulo net per connesione con rete, senza usare http
 var strftime = require('strftime')//modulo per avere date nel formato fico
 var portNumber = process.argv[2]
