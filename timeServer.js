bf08a9cc2932e343b71710bd5ddff7d940166fcf
Imran3 <imranazam3@gmail.com>
2015-03-11 11:45:39 +0100

    servo roba in html

--- timeServer.js
+++ timeServer.js
@@ -12,7 +12,10 @@ var timeServer = net.createServer(function (socket){
 	//console.log("Client connesso")
 	//console.log("mando data al cliet.")
 	//socket.write("La data di oggi è")
-	socket.write(strftime('%Y-%m-%d %H:%M\n'))
+	socket.write("<html><head><title>Benvenuto</title></head><body><h1 background='red'>Ciao Fabbbiooo</h1><h2>Sono precisamente le ")
+	socket.write(strftime("%H:%M:%S")+" e la data di oggi e'"+strftime("%d/%m/%Y"))
+	//socket.write(strftime('%Y-%m-%d %H:%M'))
+	socket.write("</h2></body></html>")
 	socket.end()
 	//listener di fine connesione con client
 	/*socket.on("end", function(){
